e61d9850c9384979f0a3c2e96db651118b40b85b
Odin Kroeger <odkr@users.noreply.github.com>
2019-01-10 17:49:18 +0100

    Formated doc properly.

--- pandoc_refheadstyle/__init__.py
+++ pandoc_refheadstyle/__init__.py
@@ -1,59 +1,54 @@
 #!/usr/local/bin/python
 # encoding=utf-8
 # pylint: disable=C0103
-"""Pandoc filter that sets a custom style for the reference section header.
+#
+# Copyright 2018, 2019 Odin Kroeger
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+"""Pandoc filter that sets a custom style for the reference section header."""
 
-Copyright (c) 2018 Odin Kroeger
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-OR OTHER DEALINGS IN THE SOFTWARE.
-"""
-import sys
-from panflute import Div, Header, Para, stringify, run_filter
+from panflute import Div, Header, Para, stringify
 
 DEFAULT_STYLE = 'Bibliography Heading'
 
-class RefHeadStyleSetter(): # pylint: disable=R0903, R1710, W0201
+
+class RefHeadStyleSetter():  # pylint: disable=R0903, R1710, W0201
     """Sets a custom style for the reference section header."""
+
     style = DEFAULT_STYLE
 
     def __call__(self, elem, doc):
-        """Sets a custom style for the reference section header.
+        r"""Set a custom style for the reference section header.
+
+        :param panflute.Element elem: A Pandoc AST element.
+        :param panflute.Doc doc: The Pandoc document that is parsed.
+        :returns: A ``Div`` with the custom style set to the value of\
+            ``reference-header-style`` or to 'Bibliography Heading'.\
+            If *elem* isn't the reference section header, returns `None`.
+        :rtype: panflute.Element or None
 
         If -- and only if -- the metadata field ``reference-section-title`` is
         set, sets the style of the reference section header to the value of
         the metadata field ``reference-header-style`` or to 'Bibliography
         Heading' if that metadata field has not been set or is empty.
 
-        Arguments:
-            ``elem`` (``panflute.Element``):
-                A Pandoc AST element.
-            ``doc`` (``panflute.Doc``):
-                The Pandoc document that is parsed.
-
-        Returns (``panflute.Element``):
-            If ``elem`` is the reference section header:
-                A ``Div`` with the custom style set to the value of
-                ``reference-header-style`` or to 'Bibliography Heading'.
-            Otherwise:
-                ``None``
-
-        See ``set_style`` on how it is determined whether a header is the
+        See :func:`set_style` on how it is determined whether a header is the
         reference section header.
         """
         try:
@@ -67,30 +62,22 @@ class RefHeadStyleSetter(): # pylint: disable=R0903, R1710, W0201
         return self(elem, doc)
 
 
-def set_style(elem, title, style): # pylint: disable=R1710
-    """Changes the style of the reference heading.
+def set_style(elem, title, style):  # pylint: disable=R1710
+    r"""Change the style of the reference heading.
 
-    Arguments:
-        ``elem`` (``panflute.Element``):
-            A Pandoc AST element.
-        ``doc`` (``panflute.Doc``):
-            The Pandoc document that is parsed.
-        ``title`` (``str``):
-            The title of the reference section.
-        ``style`` (``str``):
-            The style of the reference section header.
+    :param panflute.Element elem: A Pandoc AST element.
+    :param panflute.Doc doc: The Pandoc document that is parsed.
+    :param str title: The title of the reference section.
+    :param str style: The style of the reference section header.
+    :returns: A ``Div`` with the custom style set to the value of\
+        ``reference-header-style`` or to 'Bibliography Heading'.
+        If *elem* isn't the reference section header, returns ``None``.
+    :rtype: panflute.Element or None
 
-    Returns (``panflute.Element``):
-        If ``elem`` is the reference section header:
-            A ``Div`` with the custom style set to the value of
-            ``reference-header-style`` or to 'Bibliography Heading'.
-        Otherwise:
-            ``None``
-
-    ``elem`` is considered the reference section header if and only if:
+    *elem* is considered the reference section header if and only if:
         1. It is an instance of Header.
         2. Its ID is 'bibliography'.
-        3. Its text is the value of ``title``.
+        3. Its text is the value of *title*.
     """
     if (isinstance(elem, Header) and
             elem.identifier == 'bibliography' and
