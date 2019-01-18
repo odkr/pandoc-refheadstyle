715f44165e0c6773c0eb9f4cfd1df76476823917
Odin Kroeger <odkr@users.noreply.github.com>
2019-01-18 11:54:00 +0100

    Updated version number.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def readme(readme_fname="README.rst"):
 NAME = 'pandoc-refheadstyle'
 
 # Version of this package.
-VERSION = '0.2.1-b6.1-b0'
+VERSION = '0.2.1-rc0.1-b0'
 
 # All other metadata.
 # pylint: disable=C0330
